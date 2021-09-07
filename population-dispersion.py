2fb126d97920324815d98e4fbcc78af55ee48fc2
josephvalenti53 <89355014+josephvalenti53@users.noreply.github.com>
2021-09-07 17:50:53 -0500

    Update population-dispersion.py

--- population-dispersion.py
+++ population-dispersion.py
@@ -1,1 +1,11 @@
+import statistics
 
+print('Data Set = 1,3,4,2,6,5,3,4,5,2')
+
+pop_variance = statistics.pvariance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])
+
+print('Population Variance =', pop_variance)
+
+pop_std_dev = statistics.pstdev([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])
+
+print('Population Standard Deviation =', pop_std_dev)
